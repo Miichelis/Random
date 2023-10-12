c3e9a053c9a91b349fa15c0be824138efabf4c24
Michelis <98261980+kdoomakii@users.noreply.github.com>
2023-10-12 18:40:09 +0300

    Fibonacci Sequence

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+#Fibonacci
+
+def fibonacci(n):
+    a=0
+    b=1
+    print a;print b
+
+    for i in range(2,n):
+        s=a+b
+        a=b
+        b=s
+        if i == n-1:
+            return s
+        else:
+            print s
+f=fibonacci(100)
+print f
