94534ffd9ea0a713b2559b35b0ceb8b1f2f938f9
Michelis <98261980+kdoomakii@users.noreply.github.com>
2023-10-24 21:45:37 +0300

    slot machine.py
    3.0V

--- slot machine.py
+++ slot machine.py
@@ -1,11 +1,7 @@
-#Slot Machine
-#deposit a sert am of money by the user
-#bet on 3 lines of the slot machine
-#run out of money = stop
-#or cashout = stop
+#SLOT MACHINE
 import random
-random_things=["7","BAR","cherry"]
-
+random_things=["7","BAR"]
+start=True
 fruits=[[],
         [],
         []]
@@ -30,10 +26,16 @@ def lucky(fruits):
 
 def money_earned(fruits):
     multiplier = 1
-    if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == 'BAR') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == 'BAR') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == 'BAR')):                                                   
-        multiplier=2
+    if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == 'BAR') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == 'BAR') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == 'BAR')):
+        if random.randint(0,100) <= 50:
+             multiplier=2
+        else:
+            multiplier=1
     if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == '7') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == '7') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == '7')):
-        multiplier=3
+        if random.randint(0,100) <= 10:
+            multiplier=3
+        else:
+            multiplier=1
   
 
    
@@ -42,91 +44,150 @@ def money_earned(fruits):
     
 
 def actual_programm(user_deposit,winner,mult,fruits):
-    global c
-    spin=5
-    restart=True
-    c=0
-    winning_line=[]
-    if fruits[0][0] == fruits[0][1] == fruits[0][2]: 
-        winning_line.append(1)
-    if fruits[1][0] == fruits[1][1] == fruits[1][2]:
-        winning_line.append(2)
-    if fruits[2][0] == fruits[2][1] == fruits[2][2]:
-        winning_line.append(3)
-    multiplier = 1
-    if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == 'BAR') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == 'BAR') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == 'BAR')):
-        multiplier=2
-    if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == '7') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == '7') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == '7')):
-        multiplier=3
-    while user_deposit >= 5 and restart == True:
-        
-        bet_line=input("Give me the line that you want to bet(1-3): ")
-        print "============================="
-        while bet_line != 1 and bet_line != 2 and bet_line != 3:
-            print "Inccorect Line"
-            bet_line=input("Give me the line that you want to bet(1-3): ")
-            print "============================="
-        if c != 0:
-            for i in range(3):
-                for j in range(3):
-                    fruits[i].append(random.choice(random_things))
-            winning_line=[]
-        if fruits[0][0] == fruits[0][1] == fruits[0][2]:
+    while start==True:
+        global c
+        global user_money
+        global game_earned
+        user_money=0
+        game_earned=0
+        spin=5
+        restart=True
+        c=0
+        winning_line=[]
+        if fruits[0][0] == fruits[0][1] == fruits[0][2]: 
             winning_line.append(1)
         if fruits[1][0] == fruits[1][1] == fruits[1][2]:
             winning_line.append(2)
         if fruits[2][0] == fruits[2][1] == fruits[2][2]:
             winning_line.append(3)
-            multiplier = 1
-            if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == 'BAR') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == 'BAR') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == 'BAR')):
-                multiplier=2
-            if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == '7') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == '7') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == '7')):
+        multiplier = 1
+        if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == 'BAR') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == 'BAR') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == 'BAR')):
+            if random.randint(0,100) <= 50:
+                 multiplier=2
+            else:
+                multiplier=1
+        if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == '7') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == '7') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == '7')):
+            if random.randint(0,100) <= 10:
                 multiplier=3
-        c+=1
-        if winning_line == []:
-            print"No winners this round"
-            print "======================"
-        if bet_line in winning_line:
-            money_won=(1.2*user_deposit)*multiplier
-            print "You won",money_won
-            print "============"
-            user_deposit+=money_won
-        else:
-            print "Sadly you lost"
-            print "=============="
-        user_deposit-=spin
-        print "Your balance is",user_deposit
-        print "==================="
-        for i in range(len(fruits)):
-                print fruits[i]
-        
-        playagain=raw_input("do you want to play again (Y/N): ")
-        while playagain != "Y" and playagain != "N":
-            print "Wrong Answer"
-            playagain=raw_input("do you want to play again (Y/N): ")
-        if playagain == 'Y':
-            restart == True
-            if user_deposit < 5:
-                print "Sadly you have no money left"
-                return "Thanks for playing"
-            fruits=[[],
-                    [],
-                    []]
+            else:
+                multiplier=1
+        while user_deposit >= 5 and restart == True:
             
-                              
-        else:
-            restart == False
-            return "Thanks for playing"
-        
+            game_earned+=user_deposit
+            
+            bet_line=input("Give me the line that you want to bet(1-3): ")
+            print "============================="
+            while bet_line != 1 and bet_line != 2 and bet_line != 3:
+                print "Inccorect Line"
+                print "=============="
+                bet_line=input("Give me the line that you want to bet(1-3): ")
+                print "============================="
+            if c != 0:
+                for i in range(3):
+                    for j in range(3):
+                        fruits[i].append(random.choice(random_things))
+                winning_line=[]
+            if fruits[0][0] == fruits[0][1] == fruits[0][2]:
+                winning_line.append(1)
+            if fruits[1][0] == fruits[1][1] == fruits[1][2]:
+                winning_line.append(2)
+            if fruits[2][0] == fruits[2][1] == fruits[2][2]:
+                winning_line.append(3)
+                multiplier = 1
+            if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == 'BAR') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == 'BAR') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == 'BAR')):
+                if random.randint(0,100) <= 50:
+                    multiplier=2
+                else:
+                    multiplier=1
+            if ((fruits[0][0] == fruits[0][1] == fruits[0][2] and fruits[0][0] == fruits[0][1] == fruits[0][2] == '7') or (fruits[1][0] == fruits[1][1] == fruits[1][2] and fruits[1][0] == fruits[1][1] == fruits[1][2] == '7') or (fruits[2][0] == fruits[2][1] == fruits[2][2] and fruits[2][0] == fruits[2][1] == fruits[2][2] == '7')):
+                if random.randint(0,100) <= 10:
+                    multiplier=3
+                else:
+                    multiplier=1
+            c+=1
+            if winning_line == []:
+                print"No winners this round"
+                print "======================"
+            if bet_line in winning_line:
+                money_won=(1.2*user_deposit)*multiplier
+                print "You won",money_won
+                print "============"
+                user_money=user_money+money_won+user_deposit
+                game_earned-=money_won
+            else:
+                print "Sadly you lost your money"
+                print "=============="
+                user_money-=user_deposit
+            
+            if user_money > 0:
+                print "You're up",user_money,"$"
+                print "============"
+            else:
+                print "You're down",abs(user_money),"$"
+                print "==========="
+            for i in range(len(fruits)):
+                    print fruits[i]
+            
+            playagain=raw_input("do you want to play again (Y/N): ")
+            while playagain != "Y" and playagain != "N":
+                print "Wrong Answer"
+                playagain=raw_input("do you want to play again (Y/N): ")
+            if playagain == 'Y':
+                restart == True
+                user_deposit=input("Give the amount of money that you want to bet: ")
+                if user_deposit < 5:
+                    print "Sadly you have to add more than 5$ to play"
+                    print "=========================================="
+                    return "Thanks for playing"
+            
+                fruits=[[],
+                        [],
+                        []]                      
+            else:
+                restart == False
+                return "Thanks for playing"
 
-user_deposit=input("Give the amount of money that you want to bet: ")
-if user_deposit < 5:
-    print "Not enough money for a spin"
-else:
-    winner=lucky(fruits)
-    mult=money_earned(fruits)
-    runner=actual_programm(user_deposit,winner,mult,fruits)
-    print runner
-    print "Total money made =",c*5,"$"
+
+while start == True:        
+    print "           RULES"
+    print"============================="
+    print "YOU HAVE TO PUT > 5 $ TO PLAY"
+    print "============================="
+    print "if you win + bet line is all 7's you have 1 in 10 chances to have a 3x multiplier"
+    print "============================="
+    print "if you win + bet line is all BAR you have 5 in 10 chances for a 2x multiplier"
+    print "============================="
+    print "The amount you bet if you win gets multiplied *1.2+ the deposit amount"
+    print "============================="
+    print "IF YOU LOSE ALL YOUR MONEY GETS LOST"
+    print "============================="
+    user_deposit=input("Give the amount of money that you want to bet: ")
+    if user_deposit < 5:
+        print "Not enough money for a spin"
+    else:
+        winner=lucky(fruits)
+        mult=money_earned(fruits)
+        runner=actual_programm(user_deposit,winner,mult,fruits)
+        print runner
+        print "==============="
+        if user_money >= 0:
+            print "Total money you made",user_money,"$"
+            print "==================="
+        else:
+            print "You lost",abs(user_money),"$"
+            print "=================="
+        admin_pass_given=raw_input("Press any key to continue... ")
+        admin_pass=open("password.txt","r")
+        admin_pass=admin_pass.read()
+        if admin_pass_given == admin_pass:
+            if game_earned >= 0:
+                print "Total money made by the casino",game_earned,"$"
+            else:
+                print "Total money lost by the casino",abs(game_earned),"$"
+            finish_game=raw_input("Do you want to finish the game?(Y/N) ")
+            while finish_game not in ["Y","N"]:
+                finish_game=raw_input("Do you want to finish the game?(Y/N) ")
+                if finish_game == "Y":
+                    start=False
         
 
