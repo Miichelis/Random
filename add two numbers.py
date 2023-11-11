8e5709dad8d2888375b22324eabad509dd0f8f95
Michelis <98261980+kdoomakii@users.noreply.github.com>
2023-11-11 12:55:50 +0200

    add two numbers.py

--- add two numbers.py
+++ add two numbers.py
@@ -13,9 +13,6 @@ class Solution:
         else:
             for i in range(len(l2)-len(l1)):
                 l1.append(0)
-        
-
-                
         for i in range(len(l1)):
             if l1[i] + l2[i]+c < 10:
                 sL.append(l1[i]+l2[i]+c)
